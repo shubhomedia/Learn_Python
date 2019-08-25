348d49505c289c82a19d1d4f48f0c2652c4a1f8a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-25 14:49:50 +0600

    +new list file added +created list (mylist) +print mylist +print specific value of list Added

---
+++ list.py
@@ -0,0 +1,9 @@
+# Simple List with Values
+mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+print(mylist) # Simple Print all Values
+
+#Simple Line Break
+print("\n")
+
+print(mylist[5]) # print 6 position value of list (because list index start with 0)
