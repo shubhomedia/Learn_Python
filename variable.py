46a7f501cd50a22757525350e2ffb80596887c60
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-25 11:04:06 +0600

    +Various Data Type added. +With Comment and Concatenation. +Data Type Function.

---
+++ variable.py
@@ -0,0 +1,29 @@
+#This file is for Various type of Variables on Python programming Language.
+
+# String Type :
+name = "Shubho"
+print(name, type(name))
+
+#Integer Type :
+age = 25
+print(age, type(age))
+
+#Float Type :
+score = 58.10
+print(score, type(score))
+
+#Boolean Type :
+game_on = False;
+print(game_on, type(game_on))
+
+#Concatenation
+my_name = "my name is " + name
+print(my_name)
+
+future_age = 20 + age
+print('My Future Age will be =',future_age)
+
+new_score = 20.11 + score
+print('New Score is =',new_score)
+
+
