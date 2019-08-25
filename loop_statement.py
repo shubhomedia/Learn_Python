9a99a06cd9977760ad9746bc7df3f42ede18a1f4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-25 14:19:16 +0600

    While loop Added

--- loop_statement.py
+++ loop_statement.py
@@ -9,3 +9,15 @@ print("\n")
 #This is another loop
 for i in range(1,10,2):
     print(i)
+
+#Simple Line Break
+print("\n")
+#While Loop
+sum = 0
+num = 10
+i = 1
+while i<=num:
+    sum = sum + i
+    i = i+1 #update counter
+#print the sum
+print("The sum is", sum)
