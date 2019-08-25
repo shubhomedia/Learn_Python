4b73ad4068cca948bbcef44e9721c85d0793ef52
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-25 14:53:45 +0600

    +new function file added +created function Added

---
+++ fuction.py
@@ -0,0 +1,11 @@
+#def means define it's use for def function
+def addition (num1,num2):
+    sum_of_nums = num1 + num2
+    print(sum_of_nums)
+
+addition(2,5) # Function called and input two number
+
+#Simple Line Break
+print("\n")
+
+addition(10,100) # same function but result vary on input number.
