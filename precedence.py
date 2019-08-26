c74e74443dd50fb9f30ebcaeda6db7f6734f1e1f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-26 23:20:26 +0600

    New Precedence File added new code added.

---
+++ precedence.py
@@ -0,0 +1,15 @@
+# Python works with this precendence
+
+"""
+    1st : ()
+    2nd : **
+    3rd : *
+    4th : /
+    5th : %
+    6th : +
+    7th : -
+"""
+print(25*15*32/2.0)
+a = 25*15
+b = 33/2.0
+print(a+b)
