ed0471d7e4d2d9e227a8088e4e415cd813cbd96d
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-26 23:59:02 +0600

    new binary code added.

--- binary_operation.py
+++ binary_operation.py
@@ -1,3 +1,12 @@
 # Simple Left Shift, right Shift
 print(1 << 3)
 print(64 >> 2)
+
+#Simple Line Break
+print("\n")
+
+print(256 & 256) # out : 256
+#Simple Line Break
+print("\n")
+print(256 & 255) #out : 0
+
