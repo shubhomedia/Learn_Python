52b2b1cd9a22178e8e103712ca4172b141720f75
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-27 22:09:03 +0600

    concatenation of string code added.

--- string_manipulation.py
+++ string_manipulation.py
@@ -3,7 +3,15 @@
 string1  = 'I am a String in Python'
 string2  ="I am a String in Python"
 
+name = "Md.Owes Quruny Shubho"
+age = " 24"
+status = " Student"
+
 len(string1)
 print(len(string2))
 
 print(string1[5:14]) # print start with index of 5 and end with 14 ( a String)
+
+name_age_status = name + age + status # concatenation of string
+
+print(name_age_status)
