a8f517255d08ac5621d0386c5270d1c39d80f79a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-27 22:45:51 +0600

    special format in string

--- format_method.py
+++ format_method.py
@@ -3,3 +3,5 @@
 print("Today I Had {0} cups of {1}".format(2,"coffee"))
 print("prices: ({x},{y},{z})".format(x = 10,y = 1.50, z = 5))
 print("The {vehicle} had {0} crashes in {1} months".format(5,6, vehicle = 'car'))
+print('{:<20}'.format("text")) # create space after text
+print('{:>20}'.format("text")) # create space before text
