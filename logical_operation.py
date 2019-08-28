6b9fa2e969ba9d7966dd26b95f4f3e10039d1ad7
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-28 16:52:50 +0600

    new code added. logical operation.

--- logical_operation.py
+++ logical_operation.py
@@ -1,2 +1,11 @@
 # simple logical operation
 print(4!=2) # true
+
+a = True
+b = False
+print (a and b) # False because a is true and b is false
+print (a or b) # True
+x = True
+y = True
+
+print (x and y) # True
