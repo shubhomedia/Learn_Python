76bc4dca3f72641d733c024544c4e5c9b4513a91
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-28 18:20:24 +0600

    elif code added.

--- if_statement.py
+++ if_statement.py
@@ -12,3 +12,14 @@ if var1 <= 10 :
     print(var1,"is less then 10 or equal")
 else:
     print (var1,"is grater then 10")
+
+#elif code
+
+x = 20
+y = 10
+if x == y:
+    print("x is equal to y")
+elif x >= y:
+    print("x is grater then y")
+else:
+    print("y is grater then x")
