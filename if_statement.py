e1daced7352fbf9c4a19a24e8039cf7fe5cca011
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-28 18:16:20 +0600

    less then grater then code added.

--- if_statement.py
+++ if_statement.py
@@ -4,3 +4,11 @@ if person1=='hello' or person1 == 'hi':
     print("Hi!, How are you") # output
 else:
     print("Hey")
+
+#new code
+x = input("Enter value of : ")
+var1 = int(x)
+if var1 <= 10 :
+    print(var1,"is less then 10 or equal")
+else:
+    print (var1,"is grater then 10")
