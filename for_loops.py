093e58667219a0287bf6619d14417d34829f224c
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-28 18:37:28 +0600

    string traversal

--- for_loops.py
+++ for_loops.py
@@ -4,5 +4,13 @@ for i in range(1,10,2):
 
 # new line
 print("\n")
+
 for i in range(20,100,10):
     print(i)
+
+# new line
+print("\n")
+
+string = "i am shubho"
+for i in range(len(string)):
+    print(string[i])
