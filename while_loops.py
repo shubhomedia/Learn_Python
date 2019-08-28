419f980a4dc6721e872b62b4a7a9acf91493967f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-28 18:54:23 +0600

    while loop code added

--- while_loops.py
+++ while_loops.py
@@ -3,3 +3,13 @@ condition = 10
 while condition !=0:
     print(condition)
     condition-=1
+# break condition
+while True:
+    print("Hello")
+    break
+
+# continue condition
+for i in range(1,11):
+    if 1 == 5:
+        continue
+    print(i)
