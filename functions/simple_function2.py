0af492ccece7f52f8d254f1ae6bc70b15f9642f6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-29 23:40:53 +0600

    even or odd code added.

--- functions/simple_function2.py
+++ functions/simple_function2.py
@@ -14,3 +14,12 @@ def print_double(x):
     print(2 * x)
 
 print_double(10) # result = 20
+
+# even or odd .
+def even(x):
+    if x%2 == 0:
+        print("Yes")
+    else:
+        print("No")
+
+even(5)
