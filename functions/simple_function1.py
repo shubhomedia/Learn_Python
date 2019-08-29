0f1faba31565d829147a4187c2002342f2dc5b8b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-29 20:53:36 +0600

    function called and print in other variable

--- functions/simple_function1.py
+++ functions/simple_function1.py
@@ -4,3 +4,11 @@ def function():
     print("This is our first function")
 
 function() # function called
+
+#Function
+def returnhello():
+    return "Hello from Python!"
+
+result = returnhello() # store function on variable and called function
+
+print(result) # called variable in print
