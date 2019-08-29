1ae094716c30624cec2feb2a762ca143f72b202f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-29 21:02:59 +0600

    new add function create with input parameter

--- functions/function_with_parameters.py
+++ functions/function_with_parameters.py
@@ -3,3 +3,11 @@ def parameters(a):
     print(a)
 
 parameters("This is input parameter") # a = this is input value pass on the function
+
+#add function using parameter
+def add(x,y,z):
+    sum = x + y + z
+    return sum
+
+new_result = add(10,20,30)
+print(new_result)
