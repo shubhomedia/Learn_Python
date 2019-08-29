93f7adf935b88815d73204a1c6aa30066e1af47f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-29 21:06:16 +0600

    default value pass.

--- functions/function_with_parameters.py
+++ functions/function_with_parameters.py
@@ -11,3 +11,11 @@ def add(x,y,z):
 
 new_result = add(10,20,30)
 print(new_result)
+
+#function with default value
+def addv2(z,x=10,y=20):
+    sum = x + y + z
+    return sum
+
+result = addv2(10) # only z value passed
+print(result)
