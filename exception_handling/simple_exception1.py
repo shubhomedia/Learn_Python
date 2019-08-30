c4a4c699d3777ec868040c971fd7e8aaead4d071
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-30 17:17:26 +0600

    test case code added.

--- exception_handling/simple_exception1.py
+++ exception_handling/simple_exception1.py
@@ -22,3 +22,12 @@ try:
     RaiseException(a)
 except ValueError as e:
     print(e)
+
+#test case
+
+def TestCase(a,b):
+    assert a<b, "a is greater than b"
+try:
+    TestCase(2,1)
+except AssertionError as e:
+    print(e)
