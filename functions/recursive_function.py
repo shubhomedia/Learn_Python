f418022ad485c72fffcff2bd7d522bcbd68cc6d2
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-30 16:46:52 +0600

    new recursive function

--- functions/recursive_function.py
+++ functions/recursive_function.py
@@ -14,3 +14,13 @@ def sum(n):
         return 1
     else:
         return n + sum(n-1)
+
+#another recursive function
+def tail_sum(n,accumulator = 0):
+    if n == 0:
+        return accumulator
+    else:
+        return tail_sum(n-1,accumulator+n)
+
+print(sum(10))
+print(tail_sum(10))
