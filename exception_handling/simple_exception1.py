3e3f327f5984e2dbcd8e0998d43db5d2b42f9686
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-30 17:08:18 +0600

    integer input, value error code.

--- exception_handling/simple_exception1.py
+++ exception_handling/simple_exception1.py
@@ -3,3 +3,11 @@ try:
     a = 5/0
 except Exception as e:
     print(e)
+
+# value error
+
+try:
+    n = int(input("Enter your integer:"))
+    print(n)
+except ValueError:
+    print("That is not a integer")
