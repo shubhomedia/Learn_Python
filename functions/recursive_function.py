2e7ee0929550e0a31276d95ea1ffcc26de1861a6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-30 16:46:16 +0600

    new function recurtion  added.

--- functions/recursive_function.py
+++ functions/recursive_function.py
@@ -7,3 +7,10 @@ def factorial(n):
         return n* factorial(n-1)
 
 print(factorial(5)) # 120 output
+
+#simple recurtion
+def sum(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sum(n-1)
