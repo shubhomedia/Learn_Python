078d39175c75206c5538c8132405db7a51913912
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-31 22:46:17 +0600

    new file open + file name find, file mode information. print code.

---
+++ work_with_files/file_operation.py
@@ -0,0 +1,6 @@
+# simple file open
+file =  open("E:\\MY_DATA\\Data Science\\Learn_Python\\work_with_files\\file.txt","r")
+print("File name : " + file.name)
+print("is closed : " + str(file.closed))
+print("Model " + file.mode)
+
