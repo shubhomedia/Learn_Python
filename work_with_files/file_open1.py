3131a7a9f897e0a9ba3969e8672a01d4f288618d
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-31 22:41:59 +0600

    new line added on file. new for loop to run all line in file

--- work_with_files/file_open1.py
+++ work_with_files/file_open1.py
@@ -2,5 +2,10 @@
 file =  open("E:\\MY_DATA\\Data Science\\Learn_Python\\work_with_files\\file.txt","r")
 print(file.read())
 print(file.read(4)) # input 4 bytes
-print(file.tell())
+file.close() # close the file
+
+# loop
+file =  open("E:\\MY_DATA\\Data Science\\Learn_Python\\work_with_files\\file.txt","r")
+for line in file:
+    print(line)
 file.close() # close the file
