8815044ba488ecdfd7f48363fc0bc2533c8aa6c4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-08-31 22:36:45 +0600

    new read position

--- work_with_files/file_open1.py
+++ work_with_files/file_open1.py
@@ -1,4 +1,6 @@
 # simple file open
 file =  open("E:\\MY_DATA\\Data Science\\Learn_Python\\work_with_files\\file.txt","r")
 print(file.read())
+print(file.read(4)) # input 4 bytes
+print(file.tell())
 file.close() # close the file
