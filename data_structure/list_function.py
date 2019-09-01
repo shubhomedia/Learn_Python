b7b37a7de363a38f000105fe7f68662070e998b3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-01 22:37:21 +0600

    import functools and use reduce

--- data_structure/list_function.py
+++ data_structure/list_function.py
@@ -1,3 +1,5 @@
 #list function
 print(list(map(lambda x: x**2 + 3*x + 1,[1,2,3,4])))
 print(list(filter(lambda x: x<4,[1,2,3,4,5,4,3,2,1])))
+import functools # function tools
+print(functools.reduce(lambda x,y: x * y, [1,2,3,4]))
