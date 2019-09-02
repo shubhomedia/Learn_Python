c307ae345fd3faa5bcb0a75022d5f1a1d111dc61
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-02 22:38:11 +0600

    a is subset or not code added

--- data_structure/sets_python.py
+++ data_structure/sets_python.py
@@ -4,3 +4,6 @@ my_set1 = set(['two','three','four'])
 print(my_set1 | my_set) # union of set
 print(my_set1 ^ my_set) # inter section of set
 print(my_set1 - my_set) # Different
+
+a = my_set - my_set1
+print(a <= my_set)
