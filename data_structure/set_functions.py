9e9100b9faefb57f7c37311355021e0e2d2061f3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-02 22:41:48 +0600

    intersection and difference code added.

--- data_structure/set_functions.py
+++ data_structure/set_functions.py
@@ -2,3 +2,5 @@ my_set = set(['one','two','three','one'])
 my_set1 = set(['two','three','four'])
 
 print(set.union(my_set,my_set1))
+print(set.intersection(my_set,my_set1))
+print(set.difference(my_set,my_set1))
