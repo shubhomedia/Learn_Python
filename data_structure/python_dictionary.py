67ca60e9adc84ba4f42d8d64d47f8de5870506c8
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-02 21:35:46 +0600

    dictionary code in try exception block

--- data_structure/python_dictionary.py
+++ data_structure/python_dictionary.py
@@ -3,3 +3,8 @@ my_dictionary = {'key':'value',('K','E','Y'):5}
 my_dictionary1 = {x:x+1 for x in range(10)}
 print(my_dictionary['key'])
 print(my_dictionary1)
+
+try:
+    print(my_dictionary[1])
+except Exception as e:
+    print(e)
