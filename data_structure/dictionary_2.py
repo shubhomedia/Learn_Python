6f6b2537b2bcb8e8a3b40cf11d334d471b7adb2a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-02 21:46:09 +0600

    shallow scopy

--- data_structure/dictionary_2.py
+++ data_structure/dictionary_2.py
@@ -1,4 +1,10 @@
 # my dictionary file shallow copy
 
 my_dictionary = {'Item':'Shirt','Size':'Medium','Price':50}
+my_dictionary1 = my_dictionary
+
+print(my_dictionary)
+my_dictionary['Size']='Small'
+print(my_dictionary1)
+
 
