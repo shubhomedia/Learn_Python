95ab3ee0b29d9a7308f19339dc154cd7d3213268
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-03 23:04:59 +0600

    simple math package code added.

---
+++ packages/simple_math.py
@@ -0,0 +1,16 @@
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+pi = math.pi
+
+# Calculate C
+C = 2*pi*r
+
+# Calculate A
+A = pi*r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
