292ba267048f272deb411a3eb3138e11d0999d1e
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-05 19:52:11 +0600

    axis sum of array

--- PyCharm_virtual_envt/array_work.py
+++ PyCharm_virtual_envt/array_work.py
@@ -7,3 +7,7 @@ a=np.array([1,2,3,4,5,6,7,8,9,10])
 print(a.sum()) # sum of array
 print(a.max())# print max value
 print(a.min())# print min value
+
+b = np.array([(1,2,3),(3,4,5)])
+print(b.sum(axis=0))
+print(b.sum(axis=1))
