8d29e68740ea5756c17b2404d349f8c692cf674c
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-07 23:32:26 +0600

    box plot added.

--- PyCharm_virtual_envt/matplotlib_test.py
+++ PyCharm_virtual_envt/matplotlib_test.py
@@ -8,3 +8,8 @@ plt.title("Distribution")
 plt.xlabel("Rang")
 plt.ylabel("Amount")
 plt.show()
+
+fig2 = plt.figure('Box-plot')
+ax1 = fig2.add_subplot(1,1,1)
+ax1.boxplot([21,12,23,35,45,60,33,22,56,34,28,40,41])
+plt.show()
