f21f96c05fda90268456df9f1be79faca17e37c1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-07 23:54:20 +0600

    bar chart working...part 1

--- PyCharm_virtual_envt/matplotlib_simple1.py
+++ PyCharm_virtual_envt/matplotlib_simple1.py
@@ -1,5 +1,13 @@
+import matplotlib.pyplot as plt
+
 data = {'Player': ['Wade','James','Kobe','Curry'],
         'First': [10,10,8,12],
         'Second':[12,8,13,8],
         'Third':[15,12,8,8],
         'Fourth':[18,20,15,8]}
+fig1 = plt.figure('Stacked Bar')
+ax1 = fig1.add_subplot(1,1)
+bar_width = 0.5
+bars = [i+1 for i in range(len(data['First']))]
+ticks = [i + (bar_width/2)for i in bars]
+ax1.bar(bars,)
