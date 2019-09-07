e28a90782319dfbc5d58e899af0baab05e15807f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-07 23:29:25 +0600

    facecolor and normed added.

--- PyCharm_virtual_envt/matplotlib_test.py
+++ PyCharm_virtual_envt/matplotlib_test.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure("Histogram")
 ax = fig.add_subplot(1,1,1)
-ax.hist([21,12,23,35,45,60,33,22,56,34,28,40,41],bins=7)
+ax.hist([21,12,23,35,45,60,33,22,56,34,28,40,41],bins=7, facecolor='r',normed=False)
 
 plt.title("Distribution")
 plt.xlabel("Rang")
