4e339916b25d0530029d79d24fc471fdbee4f5c4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-11 21:49:31 +0600

    new file char count added. with code

---
+++ work_with_files/count_char.py
@@ -0,0 +1,12 @@
+def count_char(text, char):
+  count = 0
+  for c in text:
+    if c == char:
+      count += 1
+  return count
+
+filename = input("Enter a filename: ")
+with open(filename) as f:
+  text = f.read()
+
+print(count_char(text, "r"))
