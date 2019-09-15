aa0f2e71e1919ecfa75e3de15ecfad7182fe9ba1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-15 23:03:59 +0600

    output code added.

--- loops/loop_over_list.py
+++ loops/loop_over_list.py
@@ -8,3 +8,10 @@ house = [["hallway", 11.25],
 # Build a for loop from scratch
 for room, area in house:
     print("the " + str(room) + " is " + str(area) + " sqm")
+'''''
+the hallway is 11.25 sqm
+the kitchen is 18.0 sqm
+the living room is 20.0 sqm
+the bedroom is 10.75 sqm
+the bathroom is 9.5 sqm
+'''''
