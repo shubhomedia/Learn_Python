711fc1bd57a6db6f6d563c7678101b80916415cc
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-15 23:06:21 +0600

    output added to loop 3 file

--- loops/for_loop_3.py
+++ loops/for_loop_3.py
@@ -4,3 +4,11 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Code the for loop
 for index, area in enumerate(areas) :
     print("room " + str(index+1) + ": " + str(area))
+
+"""
+the hallway is 11.25 sqm
+the kitchen is 18.0 sqm
+the living room is 20.0 sqm
+the bedroom is 10.75 sqm
+the bathroom is 9.5 sqm
+"""
