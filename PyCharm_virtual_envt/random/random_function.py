b052f442979535732b17a1c358664b90bb863e43
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-18 23:39:34 +0600

    new random number code

---
+++ PyCharm_virtual_envt/random/random_function.py
@@ -0,0 +1,11 @@
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
+#sampal
+print(np.random.randint(1,7))
