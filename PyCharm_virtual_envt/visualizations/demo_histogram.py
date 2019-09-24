39f04b846e0b505a4828fe6bd99bf91d84d32cbb
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-24 21:22:30 +0600

    histogram data added to code

--- PyCharm_virtual_envt/visualizations/demo_histogram.py
+++ PyCharm_virtual_envt/visualizations/demo_histogram.py
@@ -7,3 +7,10 @@ np.random.seed(19680801)
 mu = 100  # mean of distribution
 sigma = 15  # standard deviation of distribution
 x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=1)
