1200c6d651a30aa52192351b55bcb2a5e6ddddef
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-24 21:07:01 +0600

    new box plot added

--- PyCharm_virtual_envt/visualizations/matplotlib1.py
+++ PyCharm_virtual_envt/visualizations/matplotlib1.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure("Histogram")
 ax = fig.add_subplot(1,1,1)
-ax.hist([21,12,23,35,45,60,33,22,56,34,28,40,41],bins=7,facecolor ='r',density=True)
+ax.hist([21,12,23,35,45,60,33,22,56,34,28,40,41],bins=7,facecolor ='g',density=False)
 
 plt.title("Distribution")
 plt.xlabel("Range")
 plt.ylabel("Amount")
+plt.show()
+
+#Box Plot
+fig2 = plt.figure("Box-Plot")
+ax1 = fig2.add_subplot(1,1,1)
+ax1.boxplot([21,12,23,35,45,60,33,22,56,34,28,40,41])
 plt.show()
