3a360905d198e8e7011477fd0a1a02555fe77896
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-24 21:23:47 +0600

    Tweak spacing to prevent clipping of ylabel

--- PyCharm_virtual_envt/visualizations/demo_histogram.py
+++ PyCharm_virtual_envt/visualizations/demo_histogram.py
@@ -22,3 +22,7 @@ ax.plot(bins, y, '--')
 ax.set_xlabel('Smarts')
 ax.set_ylabel('Probability density')
 ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
