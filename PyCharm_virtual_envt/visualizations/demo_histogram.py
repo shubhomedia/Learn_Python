07cd5dba346618e01b67bfb9123ba3dab8714794
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-24 21:21:23 +0600

    example data added.

--- PyCharm_virtual_envt/visualizations/demo_histogram.py
+++ PyCharm_virtual_envt/visualizations/demo_histogram.py
@@ -2,3 +2,8 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(19680801)
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
