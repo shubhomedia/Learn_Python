4093df9db0b39c8c9707385a870863d22f9b5460
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-27 21:36:13 +0600

    output code added.

--- functions/simple_input_function.py
+++ functions/simple_input_function.py
@@ -6,3 +6,10 @@ x = int(value1)
 value2 = input("Input Value Y:")
 y = int(value2)
 print("Sum of X and Y:", sum(x,y))
+
+'''
+# output :
+Input Value X:10
+Input Value Y:20
+Sum of X and Y: 30
+'''
