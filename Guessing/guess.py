e5b04ebea3bdec7c43f0152da2932022789172ed
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-27 21:42:44 +0600

    simple condition apply on code

--- Guessing/guess.py
+++ Guessing/guess.py
@@ -1,6 +1,10 @@
 import random
 hidden = random.randrange(1,20)
 print(hidden)
-
+hidden = random.randrange(1,20)
 guess = int(input("Please Enter your Guess:"))
 
+if guess == hidden:
+    print("Hit!!")
+else:
+    print("Wrong!!")
