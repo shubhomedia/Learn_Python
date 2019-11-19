ae9a48750d6de4c91ea33fef9034e85b58a5ef6d
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-19 11:35:34 +0600

    education string added

--- data_type/owes_info_in_py.py
+++ data_type/owes_info_in_py.py
@@ -1,6 +1,8 @@
 name ="Owes Quruny Shubho"
 id = "163-15-8308"
 dateofbirth ="05 March 1995"
+education1 = "S.S.C"
+education2 = "Diploma In Engineering"
 
 
 print(name,"\n",id,"\n",dateofbirth,"\n")
