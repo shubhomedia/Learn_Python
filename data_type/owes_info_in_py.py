96a838d247d892fd9ca4b2993ff9da924c592767
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-20 11:36:10 +0600

    tag 1 added

--- data_type/owes_info_in_py.py
+++ data_type/owes_info_in_py.py
@@ -3,6 +3,6 @@ id = "163-15-8308"
 dateofbirth ="05 March 1995"
 education1 = "S.S.C"
 education2 = "Diploma In Engineering"
-
+tag1 ="Android Application Developer"
 
 print(name,"\n",id,"\n",dateofbirth,"\n")
