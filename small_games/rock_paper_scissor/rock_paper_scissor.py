9f81ea8e238fc776522768c2152a93e9944935b6
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-27 11:47:52 +0600

    print text added

--- small_games/rock_paper_scissor/rock_paper_scissor.py
+++ small_games/rock_paper_scissor/rock_paper_scissor.py
@@ -1,1 +1,7 @@
 import random
+# Print multiline instruction
+# performstring concatenation of string
+print("Winning Rules of the Rock paper scissor game as follows: \n"
+                                +"Rock vs paper->paper wins \n"
+                                + "Rock vs scissor->Rock wins \n"
+                                +"paper vs scissor->scissor wins \n")
