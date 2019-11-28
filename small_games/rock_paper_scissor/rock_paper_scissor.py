cfa58b83c5a69d0d68b31b0c10304ed422dd56d0
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-28 11:48:47 +0600

    while loop introduced

--- small_games/rock_paper_scissor/rock_paper_scissor.py
+++ small_games/rock_paper_scissor/rock_paper_scissor.py
@@ -5,3 +5,14 @@ print("Winning Rules of the Rock paper scissor game as follows: \n"
                                 +"Rock vs paper->paper wins \n"
                                 + "Rock vs scissor->Rock wins \n"
                                 +"paper vs scissor->scissor wins \n")
+while True:
+    print("Enter choice \n 1. Rock \n 2. paper \n 3. scissor \n")
+
+    # take the input from user
+    choice = int(input("User turn: "))
+
+    # OR is the short-circuit operator
+    # if any one of the condition is true
+    # then it return True value
+
+    # looping until user enter invalid input
