821cee3817cafe6605c43521a5861189ea62bcd9
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-04-04 21:32:54 +0600

    new tag code added

--- data_type/owes_info_in_py.py
+++ data_type/owes_info_in_py.py
@@ -6,5 +6,10 @@ education2 = "Diploma In Engineering"
 tag1 ="Android Application Developer"
 tag2 ="Mobile Application Developer"
 tag3 ="Data Analyst"
+blank_space = ' '
 
 print(name,"\n",id,"\n",dateofbirth,"\n")
+
+
+new_tag = name + blank_space + id + blank_space + education2
+print(new_tag)
