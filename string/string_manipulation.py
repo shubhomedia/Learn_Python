d536d64edbe5d699eed65bc99c9a6d548dcf5fe0
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-06-09 12:34:38 +0600

    convert string data to float using find and slice method

---
+++ string/string_manipulation.py
@@ -0,0 +1,17 @@
+name = "Shubho"
+print(name)
+
+fruit = 'banana'
+letter = fruit[1]
+print(letter)
+# Finding Length of data
+print(len(fruit))
+
+
+text = "X-DSPAM-Confidence:    0.8475";
+start_point = text.find(" ")
+endpoint = text.find("5")
+mix = text[start_point+1:endpoint+1]
+new_data = float(mix)
+print(new_data)
+
