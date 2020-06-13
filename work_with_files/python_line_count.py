e5055211a7743b591718a607239f0560c60c1853
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-06-13 09:48:36 +0600

    new short file read code added

--- work_with_files/python_line_count.py
+++ work_with_files/python_line_count.py
@@ -4,5 +4,6 @@ for line_count in file_load:
     count = count + 1
 print("Line Count: ", count)
 
-whole_file_read = file_load.read()
-print(len(whole_file_read))
+read_short_file = open('mbox-short.txt')
+x = read_short_file.read()
+print(len(x))
