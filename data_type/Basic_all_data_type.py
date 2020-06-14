6923548f61807e37042b4c6719eb85e22f5c5163
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-06-14 15:07:38 +0600

    various type of data in python code added

---
+++ data_type/Basic_all_data_type.py
@@ -0,0 +1,30 @@
+# <class 'int'>
+data_type_int = 100
+print(type(data_type_int))
+
+#<class 'float'>
+data_type_float = 100.20
+print(type(data_type_float))
+
+# <class 'str'>
+data_type_string = 'This is 100'
+print(type(data_type_string))
+
+# <class 'bool'>
+data_type_bool = True
+print(type(data_type_bool))
+
+# <class 'list'>
+data_type_list = [10,12,14,16,20]
+print(type(data_type_list))
+
+# <class 'set'>
+data_type_set = {10,12,14,16,20}
+print(type(data_type_set))
+
+# <class 'tuple'>
+data_type_tuple = (10,12,14,16,20)
+print(type(data_type_tuple))
+
+print("Various Type of Data in Python")
+
