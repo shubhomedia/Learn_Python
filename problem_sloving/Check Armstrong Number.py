4af44725ca3a0554156c054384e661998747c101
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-09-12 23:25:19 +0600

    code done

--- problem_sloving/Check Armstrong Number.py
+++ problem_sloving/Check Armstrong Number.py
@@ -1,1 +1,8 @@
 #Check Armstrong Number
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
