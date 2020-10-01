e23755b4c338872ed7c36ffc0e07e76ebd3b6610
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2020-10-01 22:27:34 +0600

    code done

--- problem_sloving/Decimal to Binary, Octal and Hexadecimal.py
+++ problem_sloving/Decimal to Binary, Octal and Hexadecimal.py
@@ -1,1 +1,9 @@
 # Decimal to Binary, Octal and Hexadecimal
+
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
