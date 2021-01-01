398f798fd3ac1fd9af7d5f25274b55ba1ebad1d3
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2021-01-01 19:43:35 +0600

    Simple problem 1 problem added

---
+++ Python101/simple problem 1.py
@@ -0,0 +1,16 @@
+'''
+Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle,
+
+Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+
+'''
+
+
